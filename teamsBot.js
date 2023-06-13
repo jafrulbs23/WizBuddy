cabe994b9f5778dd677fa08b5dcbc376f32773c9
jafrulbs23 <jafrul.hossain@brainstation-23.com>
2023-06-13 20:01:19 +0600

    first commit

---
+++ teamsBot.js
@@ -0,0 +1,66 @@
+const { TeamsActivityHandler, CardFactory, TurnContext } = require("botbuilder");
+const rawWelcomeCard = require("./adaptiveCards/welcome.json");
+const rawLearnCard = require("./adaptiveCards/learn.json");
+const cardTools = require("@microsoft/adaptivecards-tools");
+const axios = require('axios');
+
+class TeamsBot extends TeamsActivityHandler {
+  constructor() {
+    super();
+
+    // record the likeCount
+    this.likeCountObj = { likeCount: 0 };
+
+    this.onMessage(async (context, next) => {
+      console.log("Running with Message Activity.");
+      let txt = context.activity.text;
+      const removedMentionText = TurnContext.removeRecipientMention(context.activity);
+      if (removedMentionText) {
+        // Remove the line break
+        txt = removedMentionText.toLowerCase().replace(/\n|\r/g, "").trim();
+      }
+      try {
+        const res = await axios.get(`https://wizbuddy.herokuapp.com/other/${txt}`);
+        await context.sendActivity(res.data.message);
+      } catch (error) {
+        await context.sendActivity("Service currently unavailable!!!");
+      }
+      
+    
+      // By calling next() you ensure that the next BotHandler is run.
+      await next();
+    });
+
+    // Listen to MembersAdded event, view https://docs.microsoft.com/en-us/microsoftteams/platform/resources/bot-v3/bots-notifications for more events
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        if (membersAdded[cnt].id) {
+          const card = cardTools.AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
+          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
+          break;
+        }
+      }
+      await next();
+    });
+  }
+
+
+  // Invoked when an action is taken on an Adaptive Card. The Adaptive Card sends an event to the Bot and this
+  // method handles that event.
+  async onAdaptiveCardInvoke(context, invokeValue) {
+    // The verb "userlike" is sent from the Adaptive Card defined in adaptiveCards/learn.json
+    if (invokeValue.action.verb === "userlike") {
+      this.likeCountObj.likeCount++;
+      const card = cardTools.AdaptiveCards.declare(rawLearnCard).render(this.likeCountObj);
+      await context.updateActivity({
+        type: "message",
+        id: context.activity.replyToId,
+        attachments: [CardFactory.adaptiveCard(card)],
+      });
+      return { statusCode: 200 };
+    }
+  }
+}
+
+module.exports.TeamsBot = TeamsBot;
